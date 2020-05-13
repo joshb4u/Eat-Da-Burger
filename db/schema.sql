a43723c30e5807b5f62fe25fc2a43338bad5ceb4
joshb4u <59143348+joshb4u@users.noreply.github.com>
2020-05-12 20:30:13 -0400

    updated schema, removed unwanted codes

--- db/schema.sql
+++ db/schema.sql
@@ -8,15 +8,3 @@ CREATE TABLE burgers (
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
-
-
--- USED THE FOLLOWING CODE FOR DEPLOYING TO HEROKU
--- ---------------------------------------------------
-
--- CREATE TABLE burgers (
---     id int NOT NULL AUTO_INCREMENT,
---     burgerName VARCHAR (200) NOT NULL,
---     devoured BOOLEAN DEFAULT false,
---     createdAt TIMESTAMP NOT NULL, // Need to set this to DEFAULT CURRENT TIMESTAMP
---     PRIMARY KEY (id)
---     );
